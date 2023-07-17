f1c80eaf171315f222c56f963436c506908a71a8
Devesh Jawla <devesh.jawla@gmail.com>
2023-07-17 17:04:14 +0200

    update

--- DataSplitting.jl
+++ DataSplitting.jl
@@ -13,18 +13,22 @@ rename!(df, "Gender (0=F 1=M)" => :gender)
 include("DataUtils.jl")
 
 function pool_test_maker(df::DataFrame, n_input::Int)::Tuple{Tuple{Array{Float32, 2}, Array{Int, 2}}, Tuple{Array{Float32, 2}, Array{Int, 2}}}
-	df = select(df, [:eda_scl_usiemens,:pulse_rate_bpm, :label])
-	pool, test = split_data(df)
+	# df = select(df, [:eda_scl_usiemens,:pulse_rate_bpm, :Age, :label])
+	pool, test = split_data(df, at=0.4)
     pool = Matrix{Float32}(permutedims(pool))
     test = Matrix{Float32}(permutedims(test))
     pool_x = pool[1:n_input, :]
     pool_y = pool[end, :]
-	low = findall(pool_y .<=10)
-	med = findall(x->  10 < x <= 15, pool_y)
-	high = findall(pool_y .>15)
+	low = findall(pool_y .<=8)
+	med_low = findall(x->  8 < x <= 11, pool_y)
+	med = findall(x->  11 < x <= 14, pool_y)
+	med_high = findall(x->  14 < x <= 17, pool_y)
+	high = findall(pool_y .>17)
 	pool_y[low] .= 1
-	pool_y[med] .= 2
-	pool_y[high] .= 3
+	pool_y[med_low] .= 2
+	pool_y[med] .= 3
+	pool_y[med_high] .= 4
+	pool_y[high] .= 5
 
     # pool_mean = mean(pool_x, dims=2)
     # pool_std = std(pool_x, dims=2)
@@ -34,11 +38,15 @@ function pool_test_maker(df::DataFrame, n_input::Int)::Tuple{Tuple{Array{Float32
     test_x = test[1:n_input, :]
     test_y = test[end, :]
 	low = findall(test_y .<=10)
-	med = findall(x->  10 < x <= 15, test_y)
+	med_low = findall(x->  8 < x <= 11, test_y)
+	med = findall(x->  11 < x <= 14, test_y)
+	med_high = findall(x->  14 < x <= 17, test_y)
 	high = findall(test_y .>15)
 	test_y[low] .= 1
-	test_y[med] .= 2
-	test_y[high] .= 3
+	test_y[med_low] .= 2
+	test_y[med] .= 3
+	test_y[med_high] .= 4
+	test_y[high] .= 5
     # test_x = standardize(test_x, pool_mean, pool_std)
 	# test_x = vcat(test_x, permutedims(test[n_input, :]))
 
@@ -51,7 +59,7 @@ function pool_test_maker(df::DataFrame, n_input::Int)::Tuple{Tuple{Array{Float32
     return pool, test
 end
 
-pool, test = pool_test_maker(df, 2)
+pool, test = pool_test_maker(df, 7)
 writedlm("pool_x.csv", pool[1], ',')
 writedlm("pool_y.csv", pool[2], ',')
 writedlm("test_x.csv", test[1], ',')
--- main.jl
+++ main.jl
@@ -1,39 +1,29 @@
-using Distributed
+# using Distributed
 using XGBoost
 nsteps = 1000
-num_chains = 3
-addprocs(num_chains; exeflags=`--project`)
+n_output =5
+# addprocs(num_chains; exeflags=`--project`)
 experiment_name = "supervised_learning"
-
-using Plots, Distributions, DataFrames, CSV, DelimitedFiles, Random, StatsBase, Distances, InteractiveUtils, Flux, ReverseDiff, EvalMetrics#, Gadfly, Cairo, Fontconfig
-using CUDA
+using DelimitedFiles, DataFrames, Statistics
+include("DataUtils.jl")
 pool_x = readdlm("pool_x.csv", ',', Float32)
-pool_y = readdlm("pool_y.csv", ',', Int)
+pool_y = readdlm("pool_y.csv", ',', Int).-1
 test_x = readdlm("test_x.csv", ',', Float32)
-test_y = readdlm("test_y.csv", ',', Int)
+test_y = readdlm("test_y.csv", ',', Int).-1
 
-x,y = (Array{Float32}(permutedims(pool_x)), Array{Int}(permutedims(pool_y)))
+x,y = (Array{Float64}(permutedims(pool_x)), vec(pool_y))
 
-bst = xgboost(x,y, num_round=5, max_depth=6)
-prior = (he_init, num_params)
+bst = xgboost((x, y); num_round=nsteps, max_depth=6, objective = "multi:softmax", eval_metric = "mlogloss", num_class = n_output)
 mkpath("./$(experiment_name)/convergence_statistics")
-chains, elapsed = mcmc(prior, training_data_xy, nsteps, num_chains)
-independent_param_matrix, independent_map_params = chain_stats_(prior, chains, elapsed, nsteps, num_chains, experiment_name)
 
-predictions = pred_analyzer_multiclass(test_x, independent_param_matrix)
-map_predictions = pred_analyzer_multiclass(test_x, independent_map_params)
-ŷ_test = permutedims(Int.(predictions[1,:]))
-map_ŷ_test = permutedims(Int.(predictions[1,:]))
+ŷ_test = Int.(permutedims(predict(bst, permutedims(test_x))))
 
 writedlm("./$(experiment_name)/ŷ_test.csv", ŷ_test, ',')
-writedlm("./$(experiment_name)/map_ŷ_test.csv", map_ŷ_test, ',')
 
 if n_output == 2
 	acc, mcc, f1, fpr, prec, recall, threat, cm = performance_stats(test_y, ŷ_test)
 	writedlm("./$(experiment_name)/classification_performance.csv", [["Accuracy", "MCC", "f1", "fpr", "precision", "recall", "CSI", "CM"] [acc, mcc, f1, fpr, prec, recall, threat, cm]], ',')
 else
 	acc = accuracy_multiclass(test_y, ŷ_test)
-	map_acc = accuracy_multiclass(test_y, map_ŷ_test)
 	writedlm("./$(experiment_name)/classification_performance.csv", [["Accuracy"] [acc]], ',')
-	writedlm("./$(experiment_name)/map_classification_performance.csv", [["Accuracy"] [map_acc]], ',')
 end
